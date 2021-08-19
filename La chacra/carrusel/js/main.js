a0a7a5d6d197e148b8c375371de88722dbccc8a9
JoacoGraffigna <joacograffigna@gmail.com>
2021-08-19 14:56:35 -0300

    cambios

--- La chacra/carrusel/js/main.js
+++ La chacra/carrusel/js/main.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded',() => {
   const elementosCarousel = document.querySelectorAll('.carousel');
   M.Carousel.init(elementosCarousel,{
-    duration: 15,
+    duration: 25,
     dist:-80,
     indicators:true,
     nowRap:false,
